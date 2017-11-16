78cb64a094f735ca9483958ada9142bfafd319a7
Andrew Gacek <andrew.gacek@gmail.com>
2017-11-16 14:35:29 -0600

    Thin trapezoid inequality example

--- tcob.py
+++ tcob.py
@@ -19,6 +19,7 @@ from topics.functions import *
 from topics.number_line import *
 from topics.combinatorics import *
 from scene import Scene
+from scene.zoomed_scene import ZoomedScene
 from camera import Camera
 from mobject.svg_mobject import *
 from mobject.tex_mobject import *
@@ -332,5 +333,129 @@ class TrapezoidInequality(Scene):
         self.remove(sub)
 
         self.remove(obj)
+        self.dither(5)
+        
+
+class ThinInequality(ZoomedScene):
+    def construct(self):
+        factor = 5.0
+        self.add(NumberPlane(
+            color=DARK_GREY,
+            axes_color=GREY,
+            secondary_line_ratio=factor-1
+        ))
+        def scale(n):
+            return n / factor
+        upper_y = 12
+        lower_y = 0
+        def upper_x(y):
+            return (y + 3) / 3.0
+        def lower_x(y):
+            return 2 * y / 5.0
+
+        intro = TextMobject("""
+            Consider the trapezoid: \\\\
+            $0 \leq y \leq 12$ \\\\
+            $2y \leq 5x \land 3x \leq y+3$
+        """)
+        intro.to_corner(UP+LEFT)
+        intro.add_background_rectangle()
+        self.play(Write(intro))
+        self.dither()
+
+        def fix_xy(p):
+            x, y = p
+            return (scale(x), scale(y), 0)
+        def poly(*points, **kwargs):
+            return Polygon(*map(fix_xy, points), **kwargs)
+
+        p1 = (upper_x(lower_y), lower_y)
+        p2 = (lower_x(lower_y), lower_y)
+        p3 = (lower_x(upper_y), upper_y)
+        p4 = (upper_x(upper_y), upper_y)
+        trap = poly(p1, p2, p3, p4)
+        self.play(Write(trap))
+        self.dither()
+
+        points = []
+        for y in range(lower_y, upper_y + 1):
+            for x in range(ceil(lower_x(y)), floor(upper_x(y)) + 1):
+                points.append((x, y))
+
+        A = [[1, 2], [2, 5]]
+        A_inv = np.linalg.inv(A)
+
+        def f(p):
+            x, y, _ = p
+            return (A_inv[0][0]*x + A_inv[0][1]*y, A_inv[1][0]*x + A_inv[1][1]*y, 0)
+
+        def dot(pt, **kwargs):
+            x, y = pt
+            return Dot((scale(x), scale(y), 0), radius=0.04, **kwargs)
+
+        def f_dot(pt):
+            x, y = pt
+            f_x, f_y, _ = f((x, y, 0))
+            return Dot((scale(f_x), scale(f_y), 0), radius=0.04)
+
+        def move(vm1, vm2):
+            return Transform(vm1, vm2, run_time=DEFAULT_POINTWISE_FUNCTION_RUN_TIME)
+
+        dots = [dot(pt) for pt in points]
+        self.play(*map(Indicate, dots))
+        self.dither(2)
+        self.remove(intro)
+
+        problem = TextMobject("""
+            Some values for $y$ have no solutions for $x$
+        """)
+        problem.to_corner(UP+LEFT)
+        problem.add_background_rectangle()
+        self.play(Write(problem))
+        self.dither()
+        for y in 8, 11:
+            line = Line((scale(lower_x(y)), scale(y), 0), (scale(upper_x(y)), scale(y), 0), color=RED)
+            self.play(FocusOn(line))
+            self.add(line)
+            self.dither()
+            self.remove(line)
+        self.dither(2)
+        self.remove(problem)
+
+        step2 = TextMobject("""
+            Construct a transform relative to the lower bound on $x'$
+        """)
+        step2.to_edge(LEFT).to_edge(UP)
+        step2.add_background_rectangle()
+        self.play(Write(step2))
+        self.dither(2)
+        self.remove(step2)
+
+        equations = TexMobject("""
+            x &= x' + 2y' \\\\
+            y &= 2x' + 5y' \\\\
+            \\\\
+            x' &= 5x - 2y  \\\\
+            y' &= -2x + y  \\\\
+        """)
+        equations.to_edge(LEFT).to_edge(UP)
+        equations.add_background_rectangle()
+        self.play(Write(equations))
+        self.dither(2)
+
+        dot_moves = [move(dt, f_dot(pt)) for dt, pt in zip(dots, points)]
+        self.play(ApplyPointwiseFunction(f, trap), *dot_moves)
+
+        self.dither(2)
+
+        self.remove(equations)
+        for mob in self.get_mobjects():
+            mob.target = mob.copy().scale(3)
+        self.play(*[
+            Transform(mob, mob.target)
+            for mob in self.get_mobjects()
+        ])
+        self.add(equations)
+
         self.dither(5)
         
